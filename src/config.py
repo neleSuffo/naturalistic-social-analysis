4d6fb1a166894c13f81c711572e0e18726a6cac1
neleSuffo <163427736+neleSuffo@users.noreply.github.com>
2024-04-22 19:24:38 +0200

    testing ci

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
-
 # path variable to the input data
-videos_input_path  = '/Users/nelesuffo/projects/leuphana-IPE/data/video/'
+videos_input_path = "/Users/nelesuffo/projects/leuphana-IPE/data/video/"
 
+# test commit
 
 
 # path variables for the voice-type-classifier
-vtc_audio_path = '/Users/nelesuffo/projects/leuphana-IPE/data/audio/'
-vtc_environment_path = '/Users/nelesuffo/Library/Caches/pypoetry/virtualenvs/pyannote-afeazePz-py3.8/bin/python'
-vtc_execution_file_path = '/Users/nelesuffo/projects/leuphana-IPE/src/social_interaction/language/run_vtc.py'
-vtc_output_file_path = '/Users/nelesuffo/projects/leuphana-IPE/voice_type_classifier/output_voice_type_classifier/data/all.rttm'
+vtc_audio_path = "/Users/nelesuffo/projects/leuphana-IPE/data/audio/"
+vtc_environment_path = "/Users/nelesuffo/Library/Caches/pypoetry/virtualenvs/pyannote-afeazePz-py3.8/bin/python"  # noqa: E501
+vtc_execution_file_path = "/Users/nelesuffo/projects/leuphana-IPE/src/social_interaction/language/run_vtc.py"  # noqa: E501
+vtc_output_file_path = "/Users/nelesuffo/projects/leuphana-IPE/voice_type_classifier/output_voice_type_classifier/data/all.rttm"  # noqa: E501
