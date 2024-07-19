b907cf1996291c7ff0799f5ec29aa899448d6950
neleSuffo <163427736+neleSuffo@users.noreply.github.com>
2024-07-19 15:40:48 +0200

    Update train parameters for ml server

--- src/projects/social_interactions/models/yolov5_inference/train_yolov5.py
+++ src/projects/social_interactions/models/yolov5_inference/train_yolov5.py
@@ -1,22 +1,22 @@
 import os
 import subprocess
-from src.projects.social_interactions.common.constants import YoloParameters
+from src.projects.social_interactions.common.constants import YoloParameters as Yolo
 
 # Construct the full path to the train.py script in the YOLOv5 repository
-train_script_path = os.path.join(YoloParameters.yolov5_repo_path, "train.py")
+train_script_path = os.path.join(Yolo.yolov5_repo_path, "train.py")
 
 # Construct the training command
 train_cmd = [
     "python",
     train_script_path,
     "--img",
-    str(YoloParameters.img_size),
+    str(Yolo.img_size),
     "--batch",
-    str(YoloParameters.batch_size),
+    str(Yolo.batch_size),
     "--epochs",
-    str(YoloParameters.epochs),
+    str(Yolo.epochs),
     "--data",
-    YoloParameters.data_config_path,
+    str(Yolo.data_config),
     "--project",
     "src/projects/social_interactions/outputs/yolov5/train",
     "--name",
