73dea07b35031c5d3a2b0c38f69e5128a2b870ed
neleSuffo <163427736+neleSuffo@users.noreply.github.com>
2024-08-02 17:08:10 +0200

    Update convert_to_yolo.py

--- src/projects/shared/process_data/process_annotations/convert_to_yolo.py
+++ src/projects/shared/process_data/process_annotations/convert_to_yolo.py
@@ -45,8 +45,8 @@ def convert_bbox(
     y = y * dh
     h = h * dh
     return (x, y, w, h)
-
-
+    
+    
 def save_annotations(
     annotations: list
 ) -> None:
@@ -63,13 +63,17 @@ def save_annotations(
     output_dir.mkdir(parents=True, exist_ok=True)
     file_contents = {}
 
+    #(image_id, video_id, category_id, bbox, image_file_name, video_file_name, frame_width, frame_height)
+
+
     for annotation in annotations:
-        _, _, _, category_id, bbox, image_file_name, _, image_width, image_height = annotation
+        _, _, category_id, bbox, image_file_name, _, image_width, image_height = annotation
         bbox = json.loads(bbox)
         image_size = (image_width, image_height)
         # YOLO format: category_id x_center y_center width height
         yolo_bbox = convert_bbox(image_size, bbox)
         
+        # Create a line for the text file
         line = f"{category_id} " + " ".join(map(str, yolo_bbox)) + '\n'
         # Append the line to the list of lines for the image file
         if image_file_name not in file_contents:
@@ -80,10 +84,11 @@ def save_annotations(
     for image_file_name, lines in file_contents.items():
         txt_file = output_dir / (image_file_name + '.txt')
         try:
-            with open(txt_file, 'a') as f:
+            with open(txt_file, 'w') as f:
                 f.writelines(lines)
         except IOError as e:
             logging.error(f"Failed to write to file {txt_file}: {e}")
+
 
 def main() -> None:
     logging.info("Starting the conversion process for Yolo.")
