e19668e4d0585356037119a403d24e459e5b6de1
neleSuffo <163427736+neleSuffo@users.noreply.github.com>
2024-08-05 17:50:24 +0200

    First process_videos then process_annotations

--- src/projects/shared/process_data/__main__.py
+++ src/projects/shared/process_data/__main__.py
@@ -20,8 +20,8 @@ def run_process_videos():
         print(f"Error while running process_videos: {e}")
 
 if __name__ == "__main__":
-    run_process_annotations()
     run_process_videos()
+    run_process_annotations()
     # Split the dataset into training and validation sets
     prepare_training()
     logging.info("Data preparation complete.")
