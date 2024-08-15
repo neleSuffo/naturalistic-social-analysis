d0c1cdbea0154c77626e7ae9a95da97bb7f4221b
NeleSuffo <nele.suffo@leuphana.de>
2024-08-15 21:46:19 +0200

    add more hyperparameter to yolov8 training

--- src/projects/social_interactions/models/yolo_inference/train_yolov8.py
+++ src/projects/social_interactions/models/yolo_inference/train_yolov8.py
@@ -11,10 +11,16 @@ def main():
         epochs=Yolo.epochs, 
         batch=Yolo.batch_size, 
         imgsz=Yolo.img_size, 
-        device=[0, 1], # Train on GPU 0 and 1
+        device=(0, 1), # Train on GPU 0 and 1
         project="outputs/yolov8/train",         
         name="exp",
-        classes=[0,1]  # Focus on training the 'person' and 'reflection' class
+        classes=[0,1],  # Focus on training the 'person' and 'reflection' class
+        lr0=0.001,  # Initial learning rate
+        lrf=0.01,   # Final learning rate as a fraction of the initial learning rate (e.g., 0.01 times lr0)
+        cos_lr=True,  #Apply cosine learning rate decay
+        augment=True,  # Enable data augmentation
+        patience=10,  # Set patience for early stopping
+        plots=True,  # Enable plotsy
     )
 
     print("Training results:", results)
