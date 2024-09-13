65b446b7f064cdd889897b8831f0ffb0628191c8
NeleSuffo <nele.suffo@leuphana.de>
2024-09-13 23:11:39 +0200

    Sort config and constants

--- src/projects/social_interactions/common/constants.py
+++ src/projects/social_interactions/common/constants.py
@@ -1,168 +1,79 @@
-from collections import defaultdict
 from pathlib import Path
 
-
-
-class DetectionPaths:
+class BasePaths:
     home_dir = Path("/home/nele_pauline_suffo")
-    leuphana_ipe_dir = Path(home_dir/"projects/leuphana_ipe")
     models_dir = Path(home_dir/"models")
     data_dir = Path(home_dir/"ProcessedData")
-    
-    person = Path(leuphana_ipe_dir/"outputs/yolo/")
-    face = Path(leuphana_ipe_dir/"outputs/mtcnn/")
-    #videos_input = Path(data_dir/"videos_train/") 
-    #videos_input = Path(data_dir/"videos/") 
-    videos_input = Path(data_dir/"videos_example/") 
-    images_input = Path(data_dir/"images/")
-    results = Path(leuphana_ipe_dir/"outputs")
+    output_dir = Path(home_dir/"outputs")
+    leuphana_ipe_dir = Path(home_dir/"projects/leuphana_ipe")
+    vtc_dir = Path(home_dir/"projects/voice_type_classifier")
+    strong_sort_dir = Path(home_dir/"projects/strong_sort")
+    fast_re_id_dir = Path(home_dir/"projects/fast_re_id")
+
+class DetectionPaths:    
+    person_detections_dir = Path(BasePaths.leuphana_ipe_dir/"outputs/yolo/")
+    face_detections_dir = Path(BasePaths.leuphana_ipe_dir/"outputs/mtcnn/")
+    #videos_input_dir = Path(BasePaths.data_dir//"videos_train/") 
+    #videos_input_dir = Path(BasePaths.data_dir//"videos/") 
+    videos_input_dir = Path(BasePaths.data_dir/"videos_example/") 
+    images_input_dir = Path(BasePaths.data_dir/"images/")
     # Path variable to the annotation xml files
-    annotations_folder_path = Path("../../ProcessedData/annotations/")
-    annotations_xml_path = Path("../../ProcessedData/annotations/annotations.xml")
-    annotations_individual_folder_path = Path("../../ProcessedData/annotations_individual/")
-    annotations_json_path = Path("../../ProcessedData/annotations/annotations.json")
-    annotations_db_path = Path("../../ProcessedData/databases/annotations.db")
-    combined_json_output_path = results/"combined_detections.json"
-
+    annotations_dir = Path(BasePaths.data_dir/"annotations/")
+    annotations_xml_path = Path(annotations_dir/"annotations.xml")
+    annotations_individual_dir = Path(BasePaths.data_dir/"annotations_individual/")
+    annotations_json_path = Path(annotations_dir/"annotations.json")
+    annotations_db_path = Path(BasePaths.data_dir/"databases/annotations.db")
+    combined_json_output_path = Path(BasePaths.output_dir/"combined_detections.json")
     # The file that is used to map the file names to the file ids
-    file_name_id_dict_path = Path("../../ProcessedData/file_name_to_id_dict/annotations.xml")
+    file_name_id_mapping_path = Path(BasePaths.data_dir/"file_name_to_id_dict/annotations.xml")
 
-class DetectionParameters:
-    """
-    The parameters for the detection models.
-    """
-
-    # the video file extension
-    file_extension = ".mp4"
-
-    # Every frame_step-th frame is processed
-    frame_step = 30
-    # The class of the object to detect
-    yolo_detection_class = "person"
-    mtcnn_detection_class = "face"
-    vtc_detection_class = "voice"
-    output_key_videos = "videos"
-    output_key_annotations = "annotations"
-    output_key_images = "images"
-    output_key_categories = "categories"
-
-class YoloParameters:
-    fps = 1  # the frames per second to extract from the video
-    trained_weights_path = Path(DetectionPaths.models_dir/'yolov8_trained.pt')
-    data_config = Path(DetectionPaths.leuphana_ipe_dir/"src/projects/social_interactions/models/yolo_inference/dataset.yaml")
-    labels_input = Path(DetectionPaths.data_dir/"yolo_labels")
+class YoloPaths:
+    trained_weights_path = Path(BasePaths.models_dir/'yolov8_trained.pt')
+    data_config_path = Path(BasePaths.leuphana_ipe_dir/"src/projects/social_interactions/models/yolo_inference/dataset.yaml")
+    labels_input_dir = Path(BasePaths.data_dir/"yolo_labels")
     # the path to the input folder for the yolo model
-    data_input = Path(DetectionPaths.data_dir/"yolo")
-    batch_size = 64
-    epochs = 100
-    iou_threshold = 0.35 # the intersection over union threshold
-    img_size = (320, 640) # multi scale training
-    class_id = [1,2,11]
+    data_input_dir = Path(BasePaths.data_dir/"yolo")
 
-class VTCParameters:
-    # path variables for the voice-type-classifier
-    audio_path = Path("../../ProcessedData/audio/")
-    audio_name_ending = Path("_16kHz.wav")
-    environment_path = Path( "/home/nele_pauline_suffo/.conda/envs/pyannote/bin/python")
-    execution_file_path = Path("src/projects/social_interactions/scripts/language/run_vtc.py")
-    execution_command = Path("../voice_type_classifier/apply.sh")
-    output_file_path = Path("outputs/vtc/audio/all.rttm")
-    output_path = Path("outputs/vtc")
-    repo_path = Path("../voice_type_classifier")
-    vtc_input_path = Path("../leuphana_ipe/ProcessedData/audio/")
-    df_output_file_path = Path("outputs/vtc/audio_data.pkl")
+class VTCPaths:
+    audio_dir = Path(BasePaths.data_dir/"audio")
+    python_env_path = Path(BasePaths.home_dir/".conda/envs/pyannote/bin/python")
+    script_path = Path("src/projects/social_interactions/scripts/language/run_vtc.py")
+    command_script_path = Path(BasePaths.vtc_dir/"apply.sh")
+    output_rttm_path = Path(BasePaths.output_dir/"vtc/audio/all.rttm")
+    output_dir = Path(BasePaths.output_dir/"vtc")
+    df_output_pickle = Path(BasePaths.output_dir/"vtc/audio_data.pkl")
 
-class StrongSortParameters:
-    base_folder = Path("/home/nele_pauline_suffo/ProcessedData/strong_sort/")
-    video_input = Path(f"{base_folder}/quantex/")
-    videos_train = Path(f"{video_input}/train/")
-    videos_val = Path(f"{video_input}/val/")
-    image_subfolder = "img1"
-    feature_subfolder = "features"
-    detection_file = "det/det.txt"
-    ecc_output = Path({base_folder}/"ECC_train.json")
+class StrongSortPaths:
+    base_dir = Path(BasePaths.data_dir/"strong_sort/")
+    video_input_dir = Path(base_dir/"quantex/")
+    train_videos_dir = Path(video_input_dir/"train/")
+    val_videos_dir = Path(video_input_dir/"val")
+    ecc_output_path = Path({base_dir}/"ECC_train.json")
     
-class FastReIDParameters:
-    base_folder = Path("/home/nele_pauline_suffo/ProcessedData/fast_re_id/")
-    pretrained_model = Path(DetectionPaths.models_dir/"duke_agw_R101-ibn.pth")
-    config_file = Path(DetectionPaths.leuphana_ipe_dir/"src/projects/social_interactions/models/fast_re_id/dukemtmc_agw_resnet101_ibn.yaml")
+class FastReIDPaths:
+    base_dir = Path(BasePaths.data_dir/"fast_re_id/")
+    python_env_path = Path(BasePaths.home_dir/".cache/pypoetry/virtualenvs/fastreid-GjhRcNOO-py3.8")
+    pretrained_model_path = Path(DetectionPaths.models_dir/"duke_agw_R101-ibn.pth")
+    config_file_path = Path(DetectionPaths.leuphana_ipe_dir/"src/projects/social_interactions/models/fast_re_id/dukemtmc_agw_resnet101_ibn.yaml")
+    trt_engine_path = Path(BasePaths.models_dir/"duke_R101.engine")
+    output_dir = Path(BasePaths.output_dir/"fast_reid/")
     
-class ResNetParameters:
-    epochs = 10
+class ResNetPaths:
     trained_model_path = Path('gaze_estimation_model.pth')
-    gaze_label_csv = Path('../../ProcessedData/gaze_labels.csv')
-    batch_size = 32
+    gaze_labels_csv_path = Path(BasePaths.data_dir/'gaze_labels.csv')
     
-class MtcnnParameters:
-    labels_input = Path("../../ProcessedData/mtcnn/labels.txt")
-    data_input = Path("../../ProcessedData/mtcnn/")
-    class_id = [1,2]
-
-class TrainParameters:
-    train_test_split = 0.8
-    random_seed = 42
+class MtcnnPaths:
+    data_dir = Path(BasePaths.data_dir/"mtcnn/")
+    labels_file_path = Path(data_dir/"labels.txt")
 
 class VideoParameters:
-    # The standard frame width and height
-    frame_width = 2304
-    frame_height = 1296
-    # The number of videos to process concurrently
-    batch_size = 16
-    # Define the path for the success log file 
-    # (contains the paths of the successfully processed videos)
     success_log_path = Path("src/projects/shared/process_data/output/success.log")
-
-class LabelToCategoryMapping:
-    # Map labels to integers (defaultdict is used to return 99 for unknown labels)
-    label_dict = defaultdict(
-        lambda: 99,
-        {
-            "person": 1,
-            "reflection": 2,
-            "book": 3,
-            "animal": 4,
-            "toy": 5,
-            "kitchenware": 6,
-            "screen": 7,
-            "food": 8,
-            "object": 9,
-            "other_object": 9,
-            "face": 10,
-            'child_body_parts': 11,
-            "voice": 20,
-            "noise": -1,
-        },
-    )
-
-    # Map label id to supercategory
-    supercategory_dict = defaultdict(
-        lambda: "unknown",
-        {
-            1: "person",
-            2: "reflection",
-            3: "object",
-            4: "object",
-            5: "object",
-            6: "object",
-            7: "object",
-            8: "object",
-            9: "object",
-            10: "face",
-            20: "voice",
-            -1: "noise",
-            99: "unknown",
-        },
-    )
-
-    unknown_label_id = -1
-    unknown_supercategory = "unknown"
     
-    
-class ModelsPreprocessing:
-    yolo = "yolo"
-    mtcnn = "mtcnn"
-    fast_re_id = "fast_re_id"
-    strong_sort = "strong_sort"
-    gaze_estimation = "gaze_estimation"
-    vtc = "vtc"
-    openpose = "openpose"
+class ModelNames:
+    yolo_model = "yolo"
+    mtcnn_model = "mtcnn"
+    fast_reid_model = "fast_re_id"
+    strong_sort_model = "strong_sort"
+    gaze_estimation_model = "gaze_estimation"
+    vtc_model = "vtc"
+    openpose_model = "openpose"
--- src/projects/social_interactions/config/config.py
+++ src/projects/social_interactions/config/config.py
@@ -1,4 +1,7 @@
 from dynaconf import Dynaconf
+from collections import defaultdict
+from pathlib import Path
+
 
 settings = Dynaconf(
     envvar_prefix="DYNACONF",
@@ -10,3 +13,99 @@ settings = Dynaconf(
 
 # Whether to generate the output video with the detection results
 generate_detection_output_video = settings.get("generate_detection_output_video", False)
+
+class DetectionParameters:
+    # the video file extension
+    video_file_extension = ".mp4"
+    # Every frame_step-th frame is processed
+    frame_step_interval = 30
+    # The class of the object to detect
+    yolo_detection_target = "person"
+    mtcnn_detection_target = "face"
+    vtc_detection_target = "voice"
+    output_key_videos = "videos"
+    output_key_annotations = "annotations"
+    output_key_images = "images"
+    output_key_categories = "categories"
+    
+class YoloConfig:
+    extraction_fps = 1  # the frames per second to extract from the video
+    batch_size = 64
+    num_epochs = 100
+    iou_threshold = 0.35 # the intersection over union threshold
+    img_size = (320, 640) # multi scale training
+    target_class_ids = [1,2,11]
+    
+class VTCConfig:
+    audio_file_suffix = Path("_16kHz.wav")
+
+class StrongSortConfig:
+    image_subdir = "img1"
+    feature_subdir = "features"
+    detection_file_path = "det/det.txt"
+    
+class FastReIDConfig:
+    trt_batch_size = 8
+    trt_height = 256
+    trt_width = 128
+    
+class ResNetConfig:
+    num_epochs = 10
+    batch_size = 32
+    
+class MtcnnConfig:
+    class_ids = [1,2]
+
+class TrainingConfig:
+    train_test_split_ratio = 0.8
+    random_seed = 42
+    
+class VideoConfig:
+    frame_width = 2304
+    frame_height = 1296
+    video_batch_size = 16
+
+class LabelToCategoryMapping:
+    # Map labels to integers (defaultdict is used to return 99 for unknown labels)
+    label_to_id_mapping = defaultdict(
+        lambda: 99,
+        {
+            "person": 1,
+            "reflection": 2,
+            "book": 3,
+            "animal": 4,
+            "toy": 5,
+            "kitchenware": 6,
+            "screen": 7,
+            "food": 8,
+            "object": 9,
+            "other_object": 9,
+            "face": 10,
+            'child_body_parts': 11,
+            "voice": 20,
+            "noise": -1,
+        },
+    )
+
+    # Map label id to supercategory
+    id_to_supercategory_mapping = defaultdict(
+        lambda: "unknown",
+        {
+            1: "person",
+            2: "reflection",
+            3: "object",
+            4: "object",
+            5: "object",
+            6: "object",
+            7: "object",
+            8: "object",
+            9: "object",
+            10: "face",
+            20: "voice",
+            -1: "noise",
+            99: "unknown",
+        },
+    )
+
+    unknown_label_id = -1
+    unknown_supercategory = "unknown"
