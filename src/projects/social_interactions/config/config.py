393eca6ae30b1f3915a6620a83cecdc0baaeaf91
neleSuffo <163427736+neleSuffo@users.noreply.github.com>
2024-11-04 14:42:18 +0100

    Activate dynaconf

--- src/projects/social_interactions/config/config.py
+++ src/projects/social_interactions/config/config.py
@@ -1,18 +1,19 @@
 #from dynaconf import Dynaconf
 from collections import defaultdict
 from pathlib import Path
+from dynaconf import Dynaconf
 
 
-# settings = Dynaconf(
-#     envvar_prefix="DYNACONF",
-#     settings_files=["settings.toml", ".secrets.toml"],
-# )
+settings = Dynaconf(
+    envvar_prefix="DYNACONF",
+    settings_files=["settings.toml", ".secrets.toml"],
+)
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
 # `settings_files` = Load these files in the order.
 
 # Whether to generate the output video with the detection results
-#generate_detection_output_video = settings.get("generate_detection_output_video", False)
+generate_detection_output_video = settings.get("generate_detection_output_video", True)
 
 class DetectionParameters:
     # the video file extension
