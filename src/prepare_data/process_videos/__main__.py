119f07607e86b43e9f4df725184ef6a2f54dd376
NeleSuffo <nele.suffo@leuphana.de>
2025-01-16 21:49:31 +0100

    Refactor video frame extraction to use nth frame extraction method and enhance logging

--- src/prepare_data/process_videos/__main__.py
+++ src/prepare_data/process_videos/__main__.py
@@ -1,18 +1,17 @@
 import logging
-from utils import extract_frames_from_videos 
-from constants import DetectionPaths, ModelNames
-from config import YoloConfig
+from utils import extract_every_nth_frame_from_videos_in_folder 
+from constants import DetectionPaths, VideoParameters
+from config import DetectionParameters
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def main() -> None:
-    output_dir = DetectionPaths.images_input_dir
-    # Ensure output directory exists
-    output_dir.mkdir(parents=True, exist_ok=True)
+def main() -> None: 
+    logging.info("Starting to extract frames from videos.")
     # Extract frames from video
-    extract_frames_from_videos(DetectionPaths.videos_input_dir, output_dir, YoloConfig.extraction_fps, ModelNames.yolo_model)
-
+    extract_every_nth_frame_from_videos_in_folder(DetectionPaths.quantex_videos_input_dir, DetectionPaths.images_input_dir, DetectionParameters.frame_step_interval, VideoParameters.rawframes_extraction_error_log)
+    logging.info("Finished extracting frames from videos.")
+    
 if __name__ == "__main__":
     main()
