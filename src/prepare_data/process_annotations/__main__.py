84a9673515f5e151f6e505915e826418ea036cc1
NeleSuffo <nele.suffo@leuphana.de>
2025-01-22 21:03:40 +0100

    Add setup_db parameter to main function for conditional database setup

--- src/prepare_data/process_annotations/__main__.py
+++ src/prepare_data/process_annotations/__main__.py
@@ -5,20 +5,21 @@ from prepare_data.process_annotations.convert_to_yolo import main as convert_to_
 from prepare_data.process_annotations.convert_to_mtcnn import main as convert_to_mtcnn
 from constants import DetectionPaths
 
-def main():
+def main(setup_db: bool = False):
     os.environ['OMP_NUM_THREADS'] = '10'
-    # Create a dictionary with the task name as key and a file id as valuey
-    task_file_id_dict = create_file_name_id_dict(DetectionPaths.file_name_id_mapping_path)
-    # Create a database table for the video file names and ids
-    create_db_table_video_name_id_mapping(task_file_id_dict)
-    # Convert the XML annotations to COCO format and store the results in a database
-    write_xml_to_database()
-    # Delete the erroneous videos in the database and add the new data form the indivual xml files
-    correct_erronous_videos_in_db()
-    
-    # Exclude child body parts from the class "person" in the YOLO labels
-    # Create new class "child_body_parts" and update database
-    create_child_class_in_db()
+    if setup_db == True:
+        # Create a dictionary with the task name as key and a file id as valuey
+        task_file_id_dict = create_file_name_id_dict(DetectionPaths.file_name_id_mapping_path)
+        # Create a database table for the video file names and ids
+        create_db_table_video_name_id_mapping(task_file_id_dict)
+        # Convert the XML annotations to COCO format and store the results in a database
+        write_xml_to_database()
+        # Delete the erroneous videos in the database and add the new data form the indivual xml files
+        correct_erronous_videos_in_db()
+        
+        # Exclude child body parts from the class "person" in the YOLO labels
+        # Create new class "child_body_parts" and update database
+        create_child_class_in_db()
     
     # Convert the annotations to YOLO format and MTCNN format
     convert_to_yolo("face")
