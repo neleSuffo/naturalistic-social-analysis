5fe073041a51dfde761dcd39ffdfc21996f123c5
NeleSuffo <nele.suffo@leuphana.de>
2025-01-25 19:57:11 +0100

    Update YOLO face detection training script to use a new pretrained model, increase epochs to 200, and extend patience for early stopping to 10

--- src/models/yolo_face_detection/train_yolo_face.py
+++ src/models/yolo_face_detection/train_yolo_face.py
@@ -8,12 +8,12 @@ os.environ['OMP_NUM_THREADS'] = '4'  # OpenMP threads
 torch.set_num_threads(4)  # PyTorch threads
 
 # Load the YOLO model
-model = YOLO("/home/nele_pauline_suffo/models/yolov8_github_face.pt")  # Use pretrained YOLOv8 model
+model = YOLO("/home/nele_pauline_suffo/models/yolov11_face_detection_AdamCodd.pt")  # Use pretrained YOLOv8 model
 
 # Train the model with a cosine annealing learning rate scheduler
 model.train(
     data=str(YoloPaths.face_data_config_path),
-    epochs=100,  # Total number of epochs
+    epochs=200,  # Total number of epochs
     imgsz=640,  # Image size
     batch=16,   # Batch size
     project=str(YoloPaths.face_output_dir),  # Output directory
@@ -22,7 +22,7 @@ model.train(
     lr0=0.01,  # Initial learning rate
     lrf=0.001,  # Final learning rate after scheduling
     cos_lr=True,  # Use cosine annealing for learning rate scheduling,
-    patience=5,  # Stop training if no improvement for 5 consecutive epochs
+    patience=10,  # Stop training if no improvement for 5 consecutive epochs
     device=0,  # GPU (use "cpu" for CPU training)
     plots=True,  # Plot training results
 )
