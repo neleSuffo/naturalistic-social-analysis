72ff2d0a3ca726fe06f0933aa3a5b05a5876657f
NeleSuffo <nele.suffo@leuphana.de>
2025-02-10 06:51:59 +0100

    Add command-line interface for postprocessing VTC output and specify dataset type

--- src/models/vtc/postprocess_vtc_output.py
+++ src/models/vtc/postprocess_vtc_output.py
@@ -1,3 +1,32 @@
+import argparse
+import os
 from my_utils import rttm_to_dataframe
+from constants import VTCPaths
 
-rttm_to_dataframe('/home/nele_pauline_suffo/projects/voice_type_classifier/output_voice_type_classifier/share/all.rttm')
+def main():
+    parser = argparse.ArgumentParser(
+        description="Postprocess VTC output by converting all.rttm to a DataFrame."
+    )
+    parser.add_argument(
+        "folder_path",
+        type=str,
+        help="Folder path containing the all.rttm file."
+    )
+    parser.add_argument(
+        "dataset_type",
+        type=str,
+        choices=["childlens", "quantex"],
+        help="Type of dataset: either 'childlens' or 'quantex'."
+    )
+    args = parser.parse_args()
+    
+    if args.dataset_type == "childlens":
+        output_path = VTCPaths.childlens_df_file_path
+    else:
+        output_path = VTCPaths.quantex_df_file_path
+
+    rttm_file = os.path.join(args.folder_path, "all.rttm")
+    rttm_to_dataframe(rttm_file, output_path)
+
+if __name__ == "__main__":
+    main()
