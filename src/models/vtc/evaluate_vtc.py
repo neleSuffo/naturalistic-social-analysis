5773fd6a0abfa8484c266d7c01c514d1c5231996
NeleSuffo <nele.suffo@leuphana.de>
2025-02-10 13:37:40 +0100

    Refactor VTC evaluation script to improve annotation processing; replace JSON processing function and consolidate ground truth DataFrame creation.

--- src/models/vtc/evaluate_vtc.py
+++ src/models/vtc/evaluate_vtc.py
@@ -5,15 +5,17 @@ from pathlib import Path
 from typing import Dict
 import pandas as pd
 import logging
-from my_utils import rttm_to_dataframe, process_all_json_files_to_dataframe
+from my_utils import rttm_to_dataframe, process_superannotate_annotations_to_dataframe
 from constants import VTCPaths, DetectionPaths
-from config import AnnotationProcessing
 from pyannote.metrics.detection import DetectionPrecisionRecallFMeasure
 from pyannote.core import Annotation, Segment
 
+logging.basicConfig(level=logging.INFO)
+
 def main(dataset_type: str) -> None:
     """
-    This function processes the VTC output for a given dataset and evaluates the performance for the childlens dataset.
+    This function processes the VTC output for a given dataset into a dataframe
+    and evaluates the performance for the childlens dataset.
     
     Parameters:
     ----------
@@ -32,6 +34,7 @@ def main(dataset_type: str) -> None:
     rttm_to_dataframe(rttm_file, output_path)
     
     if dataset_type == "childlens":
+        gt_df = pd.DataFrame()
         base_input_dir = DetectionPaths.childlens_annotations_dir
         # convert ground truth annotations to df
         for subfolder in base_input_dir.glob("annotations_*"):
@@ -39,18 +42,19 @@ def main(dataset_type: str) -> None:
                 logging.info(f"Processing subfolder: {subfolder}")
                 
                 # Process all JSON files and convert to DataFrame
-                combined_df = process_all_json_files_to_dataframe(subfolder)
+                combined_df = process_superannotate_annotations_to_dataframe(subfolder)
                 
-                # Generate the output file path based on the subfolder name
-                output_file = base_input_dir / f"{dataset_type}_{subfolder.name}"
+                # Add combined_df to gt_df
+                gt_df = pd.concat([gt_df, combined_df], ignore_index=True)
                 
-                # Save the DataFrame to a pickle file
-                combined_df.to_pickle(output_file.with_suffix('.pkl'))
-                logging.info(f"DataFrame saved to: {output_file.with_suffix('.pkl')}")
+            # Save the DataFrame to a pickle file
+            gt_df.to_pickle(VTCPaths.childlens_gt_df_file_path)
+            logging.info(f"DataFrame saved to: {VTCPaths.childlens_gt_df_file_path}")
+            
 
         # Evaluate the performance of the VTC output
-        reference_df = pd.read_pickle(VtcPaths.childlens_gt_df_file_path)
-        hypothesis_df = pd.read_pickle(VtcPaths.childlens_df_file_path)
+        reference_df = pd.read_pickle(VTCPaths.childlens_gt_df_file_path)
+        hypothesis_df = pd.read_pickle(VTCPaths.childlens_df_file_path)
         reference = dataframe_to_annotation(reference_df)
         hypothesis = dataframe_to_annotation(hypothesis_df)
 
