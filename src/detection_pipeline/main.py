9f255ef9d0766e9a181a72b2d1e15a8faec4c3ea
NeleSuffo <nele.suffo@leuphana.de>
2025-02-12 21:46:17 +0100

    Refactor main.py to import yolo_person_face_gaze and streamline detection pipeline execution

--- src/detection_pipeline/main.py
+++ src/detection_pipeline/main.py
@@ -1,11 +1,21 @@
-import detect_person_faces
+import subprocess
+import yolo_person_face_gaze
 from setup_detection_database import setup_detection_database
 
 def main():
-    
     # Setup the detection database which will hold the detection results
     setup_detection_database()
-    detect_person_faces.main()
+    # Run the detection pipeline
+    yolo_person_face_gaze.main()
+
+    
+    
+
+def main():
+    # Setup the detection database which will hold the detection results
+    setup_detection_database()
+    # Run the detection pipeline
+    yolo_person_face_gaze.main()
     
 if __name__ == "__main__":
     main() 
