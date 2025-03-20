923965835294d41bc0cad7d21c502e2b1ae1449d
NeleSuffo <nele.suffo@leuphana.de>
2025-03-20 11:41:28 +0100

    Update video frame extraction to prioritize Quantex videos and disable ChildLens extraction

--- src/prepare_data/process_videos.py
+++ src/prepare_data/process_videos.py
@@ -10,8 +10,8 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 def main() -> None: 
     logging.info("Starting to extract frames from videos.")
     # Extract frames from video
-    #extract_every_nth_frame_from_videos_in_folder(DetectionPaths.quantex_videos_input_dir, DetectionPaths.images_input_dir, DetectionParameters.frame_step_interval, VideoParameters.rawframes_extraction_error_log)
-    extract_every_nth_frame_from_videos_in_folder(DetectionPaths.childlens_videos_input_dir, DetectionPaths.childlens_images_input_dir, DetectionParameters.frame_step_interval, VideoParameters.rawframes_extraction_error_log)
+    extract_every_nth_frame_from_videos_in_folder(DetectionPaths.quantex_videos_input_dir, DetectionPaths.images_input_dir, DetectionParameters.frame_step_interval, VideoParameters.rawframes_extraction_error_log)
+    #extract_every_nth_frame_from_videos_in_folder(DetectionPaths.childlens_videos_input_dir, DetectionPaths.childlens_images_input_dir, DetectionParameters.frame_step_interval, VideoParameters.rawframes_extraction_error_log)
     logging.info("Finished extracting frames from videos.")
     
 if __name__ == "__main__":
