d34f8be0453de06aad65c5644c358135823fae72
NeleSuffo <nele.suffo@leuphana.de>
2025-04-01 20:43:55 +0200

    Fix audio extraction path in process_videos.py to use VTCPaths constants

--- src/prepare_data/process_videos.py
+++ src/prepare_data/process_videos.py
@@ -11,13 +11,13 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 def main() -> None: 
     logging.info("Starting to extract frames from videos.")
     # Extract frames from video
-    extract_every_nth_frame_from_videos_in_folder(DetectionPaths.quantex_videos_input_dir, DetectionPaths.images_input_dir, DetectionParameters.frame_step_interval, VideoParameters.rawframes_extraction_error_log)
+    #extract_every_nth_frame_from_videos_in_folder(DetectionPaths.quantex_videos_input_dir, DetectionPaths.images_input_dir, DetectionParameters.frame_step_interval, VideoParameters.rawframes_extraction_error_log)
     #extract_every_nth_frame_from_videos_in_folder(DetectionPaths.childlens_videos_input_dir, DetectionPaths.childlens_images_input_dir, DetectionParameters.frame_step_interval, VideoParameters.rawframes_extraction_error_log)
     logging.info("Finished extracting frames from videos.")
     
     logging.info("Starting to extract audio from videos.")
     # Extract audio from video
-    extract_audio_from_videos_in_folder(DetectionPaths.quantex_videos_input_dir, DetectionPaths.quantex_audio_dir)
+    extract_audio_from_videos_in_folder(DetectionPaths.quantex_videos_input_dir, VTCPaths.quantex_audio_dir)
     
 if __name__ == "__main__":
     main()
