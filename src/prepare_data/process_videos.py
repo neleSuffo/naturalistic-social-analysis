b59e8f69c515f81b17ff1de8e50966006574b852
NeleSuffo <nele.suffo@leuphana.de>
2025-04-03 15:50:04 +0200

    Add cropping functionality for detections after audio extraction

--- src/prepare_data/process_videos.py
+++ src/prepare_data/process_videos.py
@@ -3,6 +3,7 @@ from utils import extract_every_nth_frame_from_videos_in_folder
 from constants import DetectionPaths, VideoParameters, VTCPaths
 from config import DetectionParameters
 from utils import extract_audio_from_videos_in_folder
+from crop_detections import main as crop_detections_main
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -18,6 +19,12 @@ def main() -> None:
     logging.info("Starting to extract audio from videos.")
     # Extract audio from video
     extract_audio_from_videos_in_folder(DetectionPaths.quantex_videos_input_dir, VTCPaths.quantex_audio_dir)
+    logging.info("Finished extracting audio from videos.")
+    
+    logging.info("Starting to crop detections from labels.")
+    # extract persons and faces from the images
+    crop_detections_main()
+    logging.info("Finished cropping detections from labels.")
     
 if __name__ == "__main__":
     main()
