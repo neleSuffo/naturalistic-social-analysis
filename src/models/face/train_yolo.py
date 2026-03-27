c445880fbb2f3f560599386ad930232638bd15ef
NeleSuffo <nele.suffo@leuphana.de>
2026-03-27 18:31:38 +0100

    update device argument default to use multiple GPUs and refactor model loading in YOLO training scripts

--- src/models/face/train_yolo.py
+++ src/models/face/train_yolo.py
@@ -9,7 +9,7 @@ from config import FaceConfig
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train YOLO model for different detection tasks')
-    parser.add_argument('--device', type=str, default='1',
+    parser.add_argument('--device', type=str, default='0,1',
                       help='Device to use (e.g., "0" for GPU, "cpu" for CPU)')
     parser.add_argument('--config', type=str, default=str(FaceDetection.DATA_CONFIG_PATH),
                       help=f'Path to YOLO data config file (default: {FaceDetection.DATA_CONFIG_PATH})')
@@ -19,18 +19,16 @@ def main():
     args = parse_args()
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-    # Set thread limits
-    os.environ['OMP_NUM_THREADS'] = '12'
-    torch.set_num_threads(12)
-
     base_output_dir = FaceDetection.OUTPUT_DIR
 
     # Load the YOLO model - changed default to 'm'
     model_name = FaceConfig.MODEL_NAME
     print(f"Loading model: {model_name}")
 
-    model = YOLO(FaceDetection.FACE_MODEL_PATH)
-    
+    # huggingface yolo12 face model
+    #model = YOLO(FaceDetection.FACE_MODEL_PATH)
+    model = YOLO(f"{FaceConfig.MODEL_NAME}.pt")
+
     experiment_name = f"{FaceConfig.MODEL_NAME}_{timestamp}"
     output_dir = base_output_dir / experiment_name
 
@@ -49,9 +47,17 @@ def main():
         batch=FaceConfig.BATCH_SIZE,
         project=str(base_output_dir),
         name=experiment_name,
-        augment=True,
-        patience=20,
+        
+        patience=FaceConfig.PATIENCE,
+        workers=FaceConfig.NUM_WORKERS,
         device=args.device,
+        cos_lr=True,
+        degrees=FaceConfig.DEGREES,
+
+        augment=True,
+        mosaic=1.0,
+        scale=0.5,
+        
         plots=True,
         val=True,
         exist_ok=True,
--- src/models/person/train_yolo.py
+++ src/models/person/train_yolo.py
@@ -29,7 +29,7 @@ def get_data_constants(data_type: str):
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train YOLO model for different person data tasks')
-    parser.add_argument('--device', type=str, default='1', help='Device to use (e.g., "0" for GPU, "cpu" for CPU)')
+    parser.add_argument('--device', type=str, default='0,1', help='Device to use (e.g., "0" for GPU, "cpu" for CPU)')
     parser.add_argument('--type', choices=["detection", "classification"], default="detection", help='Type of task/data format (detection or classification)')
     parser.add_argument('--retrain', action='store_true', default=False,
                        help='Activate retrain mode using fixed IDs and hard negative files defined in PersonConfig.')
@@ -44,8 +44,8 @@ def main():
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
     # Set thread limits
-    os.environ['OMP_NUM_THREADS'] = '12'
-    torch.set_num_threads(12)
+    #os.environ['OMP_NUM_THREADS'] = '12'
+    #torch.set_num_threads(12)
 
     base_output_dir = CONSTANTS.OUTPUT_DIR
 
@@ -55,12 +55,12 @@ def main():
     else:
         # Load appropriate pre-trained model for the task type
         if args.type == "classification":
-            model = YOLO('yolo12l-cls.pt')
+            model = YOLO(f"{PersonConfig.MODEL_NAME}.pt")
             model_name = str(PersonConfig.MODEL_NAME) + "_cls"
             data_path = PersonClassification.INPUT_DIR / "images"
 
         else: 
-            model = YOLO('yolo12l.pt')
+            model = YOLO(f"{PersonConfig.MODEL_NAME}.pt")
             model_name = str(PersonConfig.MODEL_NAME)
             data_path = PersonDetection.DATA_CONFIG_PATH
 
@@ -78,9 +78,17 @@ def main():
         batch=PersonConfig.BATCH_SIZE,
         project=str(base_output_dir),
         name=experiment_name,
-        augment=True,
-        patience=20,
+        
+        patience=PersonConfig.PATIENCE,
+        workers=PersonConfig.NUM_WORKERS,
         device=args.device,
+        cos_lr=True,
+        degrees=PersonConfig.DEGREES,
+        
+        augment=True,
+        mosaic=1.0,
+        scale=0.5,
+
         plots=True,
         val=True,
         exist_ok=True,
